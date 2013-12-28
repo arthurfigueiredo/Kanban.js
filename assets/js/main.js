6ef5375483125c1e145cea263e53fcd25e121ed3
Arthur Figueiredo <=>
2013-12-28 19:49:06 -0200

    First pages commit

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+$(function(){
+
+	var boxs = $(".box");
+
+    $('.post-it').on('dragstart', function(e) {
+        e.originalEvent.dataTransfer.setData("text/plain", e.target.getAttribute('id'));
+    });
+    
+    boxs.on('dragover', function(e) {
+		e.preventDefault();
+		boxs.removeClass("drop-here");
+		$(this).addClass("drop-here");	
+    });
+	
+	boxs.on('drop', function(e) {
+		$(this).removeClass("drop-here");
+		var card = e.originalEvent.dataTransfer.getData("text/plain");
+		e.target.appendChild(document.getElementById(card));
+
+		e.preventDefault();
+	});
+
+});
