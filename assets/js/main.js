7b08cfb5966e81e3fdec33ac5062cc40695029fd
Arthur Figueiredo Nunes <arthurfn.webdev@gmail.com>
2014-01-02 20:22:48 -0200

    Verify if target has "box" class - bugfix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,26 @@
 $(function(){
 
-	var boxs = $(".box");
+        var boxs = $(".box");
 
     $('.post-it').on('dragstart', function(e) {
         e.originalEvent.dataTransfer.setData("text/plain", e.target.getAttribute('id'));
     });
     
     boxs.on('dragover', function(e) {
-		e.preventDefault();
-		boxs.removeClass("drop-here");
-		$(this).addClass("drop-here");	
+                e.preventDefault();
+                boxs.removeClass("drop-here");
+                $(this).addClass("drop-here");        
     });
-	
-	boxs.on('drop', function(e) {
-		$(this).removeClass("drop-here");
-		var card = e.originalEvent.dataTransfer.getData("text/plain");
-		e.target.appendChild(document.getElementById(card));
-
-		e.preventDefault();
-	});
+        
+        boxs.on('drop', function(e) {
+                $(this).removeClass("drop-here");
+                var card = e.originalEvent.dataTransfer.getData("text/plain");
+                
+                if($(e.target).hasClass("box")){
+                        e.target.appendChild(document.getElementById(card));
+                }
+                
+                e.preventDefault();
+        });
 
 });
