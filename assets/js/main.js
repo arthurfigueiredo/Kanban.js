e6f4f3593b729237a9fec8ca6a7e6765c9e0901c
arthurfigueiredo <arthurfn.webdev@gmail.com>
2014-01-18 18:45:34 -0200

    New feature - add note

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,20 +3,10 @@ $(function(){
     var boxs = $(".box");
     var trash = $(".trash");
     var note = $('.post-it');
+    var newNote = $(".create-new");
 
-    note.on('dragstart', function(e) {
-        e.originalEvent.dataTransfer.setData("text/plain", e.target.getAttribute('id'));
-        trash.css({
-            opacity : 0.5
-        });
-    });
-    note.on('dragend',function(e){
-        boxs.removeClass("drop-here");
-        trash.css({
-            opacity : 0.2
-        });
-        trash.removeClass("active");
-    });
+    note.on('dragstart', noteDragStart);
+    note.on('dragend', noteDragEnd);
 
     boxs.on('dragover', function(e) {
             $(this).addClass("drop-here");   
@@ -46,5 +36,29 @@ $(function(){
             e.preventDefault();
     });
 
+    newNote.click(function(){
+        var toDo = prompt("What you have to do?");
+        var note = $('<div id="card-'+(boxs.length+1)+'" class="post-it" draggable="true"><p title="Click to edit" contenteditable="true">'+toDo+'</p></div>');
+        boxs.push(note);
 
+        note.on('dragstart', noteDragStart);
+        note.on('dragend', noteDragEnd);
+
+        boxs.first().append(note);
+    });
+
+    function noteDragStart(e) {
+        e.originalEvent.dataTransfer.setData("text/plain", e.target.getAttribute('id'));
+        trash.css({
+            opacity : 0.5
+        });
+    }
+
+    function noteDragEnd(e){
+        boxs.removeClass("drop-here");
+        trash.css({
+            opacity : 0.2
+        });
+        trash.removeClass("active");
+    }
 });
