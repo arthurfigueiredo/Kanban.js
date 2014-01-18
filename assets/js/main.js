aa21aba0fa8087f230d2b50d85ed0aa5d2d78009
arthurfigueiredo <arthurfn.webdev@gmail.com>
2014-01-18 18:05:07 -0200

    New feature - delete note

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,50 @@
 $(function(){
 
-        var boxs = $(".box");
+    var boxs = $(".box");
+    var trash = $(".trash");
+    var note = $('.post-it');
 
-    $('.post-it').on('dragstart', function(e) {
+    note.on('dragstart', function(e) {
         e.originalEvent.dataTransfer.setData("text/plain", e.target.getAttribute('id'));
-    });
-    
-    boxs.on('dragover', function(e) {
-                e.preventDefault();
-                boxs.removeClass("drop-here");
-                $(this).addClass("drop-here");        
-    });
-        
-        boxs.on('drop', function(e) {
-                $(this).removeClass("drop-here");
-                var card = e.originalEvent.dataTransfer.getData("text/plain");
-                
-                if($(e.target).hasClass("box")){
-                        e.target.appendChild(document.getElementById(card));
-                }
-                
-                e.preventDefault();
+        trash.css({
+            opacity : 0.5
         });
+    });
+    note.on('dragend',function(e){
+        boxs.removeClass("drop-here");
+        trash.css({
+            opacity : 0.2
+        });
+        trash.removeClass("active");
+    });
+
+    boxs.on('dragover', function(e) {
+            $(this).addClass("drop-here");   
+            e.preventDefault();
+    });
+    boxs.on('dragleave',function(){
+        $(this).removeClass("drop-here");
+    });
+    trash.on('dragover', function(e) {
+        e.preventDefault();
+        trash.addClass("active");
+    });
+
+
+    boxs.on('drop', function(e) {
+            var card = e.originalEvent.dataTransfer.getData("text/plain");
+            e.target.appendChild(document.getElementById(card));
+            
+            e.preventDefault();
+    });
+
+    trash.on('drop', function(e) {
+            var card = e.originalEvent.dataTransfer.getData("text/plain");
+            if(confirm("Want to delete this note?")){
+                $("#"+card).remove();
+            }
+            e.preventDefault();
+    });
+
 
 });
