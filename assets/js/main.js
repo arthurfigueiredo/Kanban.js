060b7078b1f1937765e1cc2e0453a6a57a394f39
arthurfigueiredo <arthurfn.webdev@gmail.com>
2014-01-18 21:01:33 -0200

    New feature - localStorage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,18 @@
 $(function(){
+    
+    var saveApplication = function(){
+        localStorage.setItem('app', $(".main-content").html());
+        console.log($(".main-content").html());
+    }
+    var getApplication = function(){
+        return localStorage.getItem('app'); 
+    }
+
+    var runApplication = (function(reboot){
+        if(getApplication()){
+            $(".main-content").html(getApplication());
+        }
+    })();
 
     var boxs = $(".box");
     var trash = $(".trash");
@@ -24,7 +38,6 @@ $(function(){
     boxs.on('drop', function(e) {
             var card = e.originalEvent.dataTransfer.getData("text/plain");
             e.target.appendChild(document.getElementById(card));
-            
             e.preventDefault();
     });
 
@@ -32,10 +45,16 @@ $(function(){
             var card = e.originalEvent.dataTransfer.getData("text/plain");
             if(confirm("Want to delete this note?")){
                 $("#"+card).remove();
+                saveApplication();
             }
             e.preventDefault();
     });
-
+    trash.click(function(){
+        if(confirm("Want to clear?")){
+            localStorage.setItem("app",'<div class="col-12"><div class="operations"><div class="create-new"><img src="https://cdn1.iconfinder.com/data/icons/Mobile-Icons/128/07_note.png" alt="Create new" width="85" title="Create new note"></div><div class="trash" style="opacity: 0.2;"><img src="https://cdn3.iconfinder.com/data/icons/linecons-free-vector-icons-pack/32/trash-128.png" alt="Trash" width="60"></div></div></div><div class="col-4"><div class="box"><h3>To Do</h3></div></div><div class="col-4"><div class="box"><h3>In Progress</h3></div></div><div class="col-4"><div class="box"><h3>Done</h3></div></div>');            
+            $(".main-content").html(getApplication());
+        }
+    });
     newNote.click(function(){
         var toDo = prompt("What you have to do?");
 
@@ -47,6 +66,7 @@ $(function(){
             note.on('dragend', noteDragEnd);
 
             boxs.first().append(note);
+            saveApplication();
         }
     });
 
@@ -63,5 +83,10 @@ $(function(){
             opacity : 0.2
         });
         trash.removeClass("active");
+        saveApplication();
     }
+    $(".post-it").on("keyup",function(){
+        saveApplication();
+    });
+
 });
